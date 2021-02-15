fffc7fd81b3d34057d9607f1da8846844af9ca74
arsenic-man <aryasianati@gmail.com>
2021-02-15 15:24:14 +0330

    ip founder

---
+++ my_ip.py
@@ -0,0 +1,13 @@
+import requests
+from colorama import Fore, Back, Style 
+# from termcolor import colored, cprint 
+
+ip = requests.get("https://api.myip.com").json()
+
+ip_num = ip["ip"]
+ip_country = ip["country"]
+ip_country_code = ip["cc"]
+
+print('Your IP is :', ip_num) 
+print('Your country is :', ip_country) 
+print('Your country code is :', ip_country_code)
