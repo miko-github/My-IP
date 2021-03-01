b73e2e7f0c179be9d449ac756f78d4e5d6f66ac5
mikoloism <mikoloism.github@gmail.com>
2021-03-01 12:26:00 +0330

    Pretty this
    Just change many practise 🤗

--- my_ip.py
+++ my_ip.py
@@ -1,12 +1,16 @@
 import requests
 
 
-ip = requests.get("https://api.myip.com").json()
+ipObj = requests.get("https://api.myip.com").json()
 
-ip_num = ip["ip"]
-ip_country = ip["country"]
-ip_country_code = ip["cc"]
+ip, country, cc = ipObj
 
-print('Your IP is :', ip_num) 
-print('Your country is :', ip_country) 
-print('Your country code is :', ip_country_code)
+print('{\n\tip_address: {0},\n\tcountry: {1},\n\tcountry_code: {2}\n}'.format(ip,county,cc))
+
+# ip_num = ip["ip"]
+# ip_country = ip["country"]
+# ip_country_code = ip["cc"]
+
+# print('Your IP is :', ip_num) 
+# print('Your country is :', ip_country) 
+# print('Your country code is :', ip_country_code)
